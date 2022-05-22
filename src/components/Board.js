0d83ab0e9fb7c43ca6aa144db0a87d88dc4c96e6
JCBASE <jessica.c@bronxsoftware.org>
2022-05-21 21:57:22 -0400

    made board css and made board container, rows, and squares

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,58 @@
+import React from 'react';
+import '../css/board.css';
 
 const Board = (props) => {
-    return(
-        <div>
-            <h1>Wordle Board</h1>
+
+    <h1>{props.pressedKey}</h1>
+
+    return (
+        <div id = "board-container">
+        <div id = "board" classname = "">
+            <div class = "row">
+            <div className = "square"></div>
+            <div className = "square"></div>
+            <div className = "square"></div>
+            <div className = "square"></div>
+            <div className = "square"></div>
+         </div>
+        <div class = "row">
+            <div className = "square"></div>
+            <div className = "square"></div>
+            <div className = "square"></div>
+            <div className = "square"></div>
+            <div className = "square"></div>
         </div>
+        <div class = "row">
+            <div className = "square"></div>
+            <div className = "square"></div>
+            <div className = "square"></div>
+            <div className = "square"></div>
+            <div className = "square"></div>
+        </div>
+        <div class = "row">
+            <div className = "square"></div>
+            <div className = "square"></div>
+            <div className = "square"></div>
+            <div className = "square"></div>
+            <div className = "square"></div>
+        </div>
+        <div class = "row">
+            <div className = "square"></div>
+            <div className = "square"></div>
+            <div className = "square"></div>
+            <div className = "square"></div>
+            <div className = "square"></div>
+        </div>
+        <div class = "row">
+            <div className = "square"></div>
+            <div className = "square"></div>
+            <div className = "square"></div>
+            <div className = "square"></div>
+            <div className = "square"></div>
+        </div>
+    </div>
+    </div>
+
     )
 }
 
